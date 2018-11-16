599e59f2648136264255c3cba28d99292c58cd21
Aaron Meese <ajmeese7@gmail.com>
2018-11-15 20:34:05 -0600

    Added Steam modifications
    And added some more TODOs

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # random-wallpaper
 Random background images for Wallpaper Engine
+
+Available for Wallpaper Engine on Steam [here](https://steamcommunity.com/sharedfiles/filedetails/?id=1565912187).
--- randomWallpaper.js
+++ randomWallpaper.js
@@ -2,4 +2,5 @@ function randomWallpaper() {
   var url = "https://source.unsplash.com/random/1920x1080";
   var image = document.getElementById("image");
   image.src = url; // TODO: Save URL to text file after image is gotten; replace if user keeps `random` option on
+  // TODO: Add an option some way to save displayed images! (or at least save the link)
 }
