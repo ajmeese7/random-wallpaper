524dabe407129395a04a1340ecc1404e498c5a34
Aaron Meese <ajmeese7@gmail.com>
2019-03-10 11:47:17 -0500

    Added 8-bit option
    Converts the downloaded images to 8-bit, if the user prefers.

--- randomWallpaper.js
+++ randomWallpaper.js
@@ -1,6 +1,16 @@
-function randomWallpaper() {
+// Had to change eightBit to pixel because otherwise it overrides the 8bit.js function
+function randomWallpaper(pixelDensity) {
   var url = "https://source.unsplash.com/random/1920x1080";
-  var image = document.getElementById("image");
-  image.src = url; // TODO: Save URL to text file after image is gotten; replace if user keeps `random` option on
+  var img = new Image();
+
+  img.src = url;
+  img.width = "1920";
+  img.height = "1080";
+
+  img.onload = function() {
+    eightBit(document.getElementById('canvas'), img, pixelDensity)
+  };
+
+  // TODO: Save URL to text file after image is gotten; replace if user keeps `random` option on
   // TODO: Add an option some way to save displayed images! (or at least save the link)
 }
--- wp-engine.js
+++ wp-engine.js
@@ -4,16 +4,14 @@
    ################################################ */
 window.wallpaperPropertyListener = {
     applyUserProperties: function(properties) {
-        if (properties.random) {
-            // IDEA: Add sizing option and put as parameter; ex select `4K` from dropdown and get UHD wallpapers
-            var random = properties.random.value;
-            if (random) {
-              randomWallpaper();
-            }
-
-            // NOTE: The intended purpose of this is for a new image to be generated if the user checks
-            //       then unchecks the box, but currently that isn't how it's working. The workaround I am
-            //       using until this is fixed is switching wallpapers then coming back
+        // IDEA: Add sizing option and put as parameter; ex select `4K` from dropdown and get UHD wallpapers
+        // TODO: Add a way to generate a new wallpaper without switching...
+        var eightBit = properties.eightBit.value;
+        var pixelDensity = properties.pixelDensity.value;
+        if (eightBit) {
+          randomWallpaper(pixelDensity);
+        } else {
+          randomWallpaper(100);
         }
     }
 };
