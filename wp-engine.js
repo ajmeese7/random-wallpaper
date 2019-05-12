69a85d3700d642cce37d983f4c9d16da36bc20e6
Aaron Meese <ajmeese7@gmail.com>
2019-05-12 12:42:48 -0500

    Working except for pixelation
    The refresh button is finally doing everything it was intended to do; however, when you press the button, the user's pixelation slider value is not retrieved. This means it works properly if they do not change the slider between times that the wallpaper is refreshed, but if the slider is changed the images will not reflect it unless you change to a different Wallpaper Engine wallpaper then switch back.

--- wp-engine.js
+++ wp-engine.js
@@ -5,15 +5,8 @@
 window.wallpaperPropertyListener = {
     applyUserProperties: function(properties) {
         // IDEA: Add sizing option and put as parameter; ex select `4K` from dropdown and get UHD wallpapers
-        // TODO: Add a way to generate a new wallpaper without switching...
         var eightBit = properties.eightBit.value;
         var pixelDensity = properties.pixelDensity.value;
-        // IDEA: Have this value be passed to pixelation-setting function when changed
-          // But how to monitor for change?
-
-        if (pixelDensity) {
-          setPixelation(pixelDensity);
-        }
 
         if (eightBit) {
           setPixelation(pixelDensity);
@@ -22,17 +15,19 @@ window.wallpaperPropertyListener = {
           setPixelation(100);
           randomWallpaper();
         }
-
-        /*if (hasBeenRun()) {
-          if (eightBit) {
-            setPixelation(pixelDensity);
-            changeWallpaper();
-          } else {
-            setPixelation(100);
-            changeWallpaper();
-          }
-        } else {
-          firstRun();
-        }*/
     }
 };
+
+// IDEA: Have this value be passed to pixelation-setting function when changed
+  // But how to monitor for change?
+/*
+var eightBit, pixelDensity;
+function getPixelation() {
+  alert("eightBit:" + eightBit + ", pixelDensity: " + pixelDensity);
+  if (eightBit) {
+    return pixelDensity;
+  } else {
+    return 100;
+  }
+}
+*/
